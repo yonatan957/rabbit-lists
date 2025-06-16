f0a756e4a09c951cb4dbf39d01822dceeb381970
yonatan957 <yonatan22660@gmail.com>
2025-06-16 17:55:15 +0300

    add env, add dockerfile, add condition if name == main

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import pika
+import os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue="input_queue")
-
-channel.queue_declare(queue='output_queue')
-
-channel.exchange_declare(exchange='exercise_exchange', exchange_type='direct')
-channel.queue_bind(queue='input_queue', exchange='exercise_exchange', routing_key='input_queue')
-channel.queue_bind(queue='output_queue', exchange='exercise_exchange', routing_key='output_queue')
-
+INPUT_QUEUE_NAME = os.getenv("INPUT_QUEUE_NAME", "input_queue")
+OUTPUT_QUEUE_NAME = os.getenv("OUTPUT_QUEUE_NAME", "output_queue")
+EXCHANGE_NAME = os.getenv("EXCHANGE_NAME", "exercise_exchange")
 
 class StreamAdapter:
         def __init__(self):
@@ -47,37 +40,49 @@ class StreamAdapter:
 
                 return chunks
 
+def make_call_back(channel_to_rabbit):
+        def callback(ch, method, properties, body):
+                """
+                the callback for the input queue's cousume
+                :param ch: the chanel where the message came from
+                :param method: object with information about the message and her context
+                :param properties: object with extra info that the producer can add to the message like tags or id
+                :param body: the content of the message
+                :return: none
+                """
+                stream_adapter = StreamAdapter()
 
-def callback(ch, method, properties, body):
-        """
-        the callback for the input queue's cousume
-        :param ch: the chanel where the message came from
-        :param method: object with information about the message and her context
-        :param properties: object with extra info that the producer can add to the message like tags or id
-        :param body: the content of the message
-        :return: none
-        """
-        stream_adapter = StreamAdapter()
+                message_str = body.decode()
 
-        message_str = body.decode()
+                numbers_str = message_str.split(',')
 
-        numbers_str = message_str.split(',')
+                numbers = list(map(int, numbers_str))
 
-        numbers = list(map(int, numbers_str))
+                expanded_chunks = stream_adapter.GetStreamChunks(numbers)
 
-        expanded_chunks = stream_adapter.GetStreamChunks(numbers)
+                for chunk in expanded_chunks:
+                        message = ','.join(map(str, chunk))
 
-        for chunk in expanded_chunks:
-                message = ','.join(map(str, chunk))
+                        channel_to_rabbit.basic_publish(
+                                exchange=EXCHANGE_NAME,
+                                routing_key=OUTPUT_QUEUE_NAME,
+                                body=message
+                        )
 
-                channel.basic_publish(
-                        exchange='exercise_exchange',
-                        routing_key='output_queue',
-                        body=message
-                )
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+        return callback
 
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+if __name__ == '__main__':
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
 
-channel.basic_consume(queue='input_queue', on_message_callback=callback)
+        channel.queue_declare(queue=INPUT_QUEUE_NAME)
+        channel.queue_declare(queue=OUTPUT_QUEUE_NAME)
+        channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='direct')
 
-channel.start_consuming()
+        channel.queue_bind(queue=INPUT_QUEUE_NAME, exchange=EXCHANGE_NAME, routing_key=INPUT_QUEUE_NAME)
+        channel.queue_bind(queue=OUTPUT_QUEUE_NAME, exchange=EXCHANGE_NAME, routing_key=OUTPUT_QUEUE_NAME)
+
+        callback_fn = make_call_back(channel)
+        channel.basic_consume(queue=INPUT_QUEUE_NAME, on_message_callback=callback_fn)
+        channel.start_consuming()
