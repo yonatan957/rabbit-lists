f3a1c42fd6ece7c05eee191916bebdc0e5c02638
yonatan957 <yonatan22660@gmail.com>
2025-06-16 15:55:15 +0300

    add summary

--- app.py
+++ app.py
@@ -12,15 +12,21 @@ def expand_list(list_of_nums):
         Expands the input list of numbers up to length 12 by adding alternating 1s and 2s.
         If the last number is 1,it adds 2 to the next list.
 
-        Args:
-            list_of_nums (list of int): The original list of numbers.
+        :param list_of_nums: (list of int) The original list of numbers.
 
-        Returns:
-            list of int: The extended list with length 12.
+        :returns: list of int: The extended list with length 12.
         """
         return list_of_nums
 
 def callback(ch, method, properties, body):
+        """
+        the callback for the input queue's cousume
+        :param ch: the chanel where the message came from
+        :param method: object with information about the message and her context
+        :param properties: object with extra info that the producer can add to the message like tags or id
+        :param body: the content of the message
+        :return: none
+        """
         message_str = body.decode()
 
         numbers_str = message_str.split(',')
