6073e8ffef26616b7152cf4c87a2df2e7c93aa5e
yonatan957 <yonatan22660@gmail.com>
2025-06-16 16:27:22 +0300

    create logic for add numbers to list

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ channel.queue_declare(queue="input_queue")
 
 channel.queue_declare(queue='output_queue')
 
+channel.exchange_declare(exchange='exercise_exchange', exchange_type='direct')
+channel.queue_bind(queue='input_queue', exchange='exercise_exchange', routing_key='input_queue')
+channel.queue_bind(queue='output_queue', exchange='exercise_exchange', routing_key='output_queue')
+
+last_added = 2
 def expand_list(list_of_nums):
         """
         Expands the input list of numbers up to length 12 by adding alternating 1s and 2s.
@@ -16,6 +21,24 @@ def expand_list(list_of_nums):
 
         :returns: list of int: The extended list with length 12.
         """
+        global last_added
+
+        needed = 12 - len(list_of_nums)
+
+        if last_added == 1 and needed > 0:
+                list_of_nums.insert(0, 2)
+                needed -= 1
+                last_added = 2
+
+        if needed <= 0:
+                return list_of_nums
+
+        next_num = 1
+        for i in range(needed):
+                list_of_nums.append(next_num)
+                next_num = 1 if (next_num == 2) else 2
+
+        last_added = 1 if (next_num == 2) else 2
         return list_of_nums
 
 def callback(ch, method, properties, body):
@@ -38,11 +61,11 @@ def callback(ch, method, properties, body):
 
         new_message = ','.join(map(str, expanded_list))
 
-        channel.basic_publish(exchange='',
+        channel.basic_publish(exchange='exercise_exchange',
                               routing_key='output_queue',
                               body=new_message)
 
-        ch.basic_ack(delivery_tag=method.delivry_tag)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(queue='input_queue', on_message_callback=callback)
 
