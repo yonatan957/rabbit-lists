2146afff9f6dea76b26762756b5fe39e7f139565
yonatan957 <yonatan22660@gmail.com>
2025-06-17 16:46:14 +0300

    change the convert_sequences to be generic

--- app/stream_adapter.py
+++ app/stream_adapter.py
@@ -1,20 +1,36 @@
-from typing import List
+from typing import List, Optional
 class StreamAdapter:
-    def __init__(self, chunk_size:int):
+    def __init__(
+            self,
+            chunk_size:int,
+            search_list: Optional[List[int]],
+            replace_list: Optional[List[int]]):
         self.last_added = 2
         self.chunk_size = chunk_size
+        self.search_list = search_list if search_list is not None else [5,5,5]
+        self.replace_list = replace_list if replace_list is not None else [6,6,6]
 
-    @staticmethod
-    def convert_5_sequences(message: List[int]) -> List[int]:
+
+    def change_search_list(self, new_search_list:List[int]):
+        self.search_list = new_search_list
+
+
+    def change_replace_list(self, new_replace_list):
+        self.replace_list = new_replace_list
+
+
+
+    def convert_5_sequences(self, message: List[int]) -> List[int]:
         i = 0
         while i <= len(message) - 3:
             triplet = message[i:i + 3]
-            if triplet == [5, 5, 5]:
-                message[i:i + 3] = [6, 6, 6]
+            if triplet == self.search_list:
+                message[i:i + 3] = self.replace_list
                 i += 3
             else:
                 i += 1
         return message
+
 
     def add_numbers_to_last_chunk(self, chunks:List[List[int]]) -> List[List[int]]:
         last_chunk = chunks[-1]
@@ -34,17 +50,19 @@ class StreamAdapter:
             self.last_added = 1 if next_num == 2 else 2
             chunks[-1] = last_chunk
         return chunks
-    def GetStreamChunks(self, message_from_queue: List[int]) -> List[List[int]]:
+
+
+    def GetStreamChunks(self, numbers_to_chunk: List[int]) -> List[List[int]]:
         """
         Expands the input list of numbers up to length 12 by adding alternating 1s and 2s.
         If the last number is 1,it adds 2 to the next list.
 
-        :param message_from_queue: (list of int) The original list of numbers.
+        :param numbers_to_chunk: (list of int) The original list of numbers.
 
         :return: list[list[int]] - List of 12-length number chunks
         """
 
-        message = self.convert_5_sequences(message_from_queue)
+        message = self.convert_5_sequences(numbers_to_chunk)
 
         chunks = []
         for i in range(0, len(message), self.chunk_size):
