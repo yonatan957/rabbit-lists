7e039863dbff6aa7d5fd0fb64db02a89ff12ca22
yonatan957 <yonatan22660@gmail.com>
2025-06-17 11:19:02 +0300

    create second test for callback

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,33 @@
-from app.app import StreamAdapter
-
+from app.app import StreamAdapter, make_call_back, EXCHANGE_NAME, OUTPUT_QUEUE_NAME
+from unittest.mock import Mock
 
 def test_stream_adapter():
     sa = StreamAdapter()
     result = sa.GetStreamChunks([1,2,3])
     assert result == [[1,2,3,1,2,1,2,1,2,1,2,1]]
+
+def test_callback():
+    fake_channel = Mock()
+    callback = make_call_back(fake_channel)
+
+    ch = Mock()
+    method = Mock()
+    method.delivery_tag = "dummy_tag"
+    properties = Mock()
+
+    body = b'1,2,3'
+
+    callback(ch, method, properties, body)
+
+    assert fake_channel.basic_publish.called
+    publish_calls = fake_channel.basic_publish.call_args_list
+
+    assert len(publish_calls) > 0
+
+    for call in publish_calls:
+        args, kwargs = call
+        assert kwargs["exchange"] == EXCHANGE_NAME
+        assert kwargs["routing_key"] == OUTPUT_QUEUE_NAME
+        assert isinstance(kwargs["body"], str)
+
+    ch.basic_ack.assert_called_once_with(delivery_tag="dummy_tag")
